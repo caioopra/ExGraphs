d3350f0dcd79eea6a0ac9c1efe252a0c58dcb52f
caioopra <caioprsilva@gmail.com>
2025-03-20 21:01:57 -0300

    feat: update Vertex struct to initialize neighbors and degree, enhance add_neighbor function

--- lib/graph/vertex.ex
+++ lib/graph/vertex.ex
@@ -3,16 +3,16 @@ defmodule ExGraphs.Vertex do
   A struct representing a vertex in a graph.
   """
   @enforce_keys [:index, :label]
-  defstruct [:index, :label, :neighbors, :degree]
+  defstruct [:index, :label, neighbors: [], degree: 0]
 
+  @doc """
+  Adds an undirected edge between two vertices.
+  Returns the updated vertices, ensuring each references the updated version of the other.
+  """
   def add_neighbor(%ExGraphs.Vertex{} = u, %ExGraphs.Vertex{} = v) do
-    add_neighbor_to_vertex(u, u)
-    add_neighbor_to_vertex(v, v)
-  end
+    updated_u = %ExGraphs.Vertex{u | neighbors: [v.index | u.neighbors], degree: u.degree + 1}
+    updated_v = %ExGraphs.Vertex{v | neighbors: [u.index | v.neighbors], degree: v.degree + 1}
 
-  defp add_neighbor_to_vertex(vertex, neighbor) do
-    vertex
-    |> Map.update(:neighbors, [neighbor], &(&1 ++ [neighbor]))
-    |> Map.update(:degree, 1, &(&1 + 1))
+    {updated_u, updated_v}
   end
 end
