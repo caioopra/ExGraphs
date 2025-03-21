2030bbe500ef508288021e525451441b662adb8e
caioopra <caioprsilva@gmail.com>
2025-03-20 21:01:26 -0300

    feat: basic struct of Graph

--- lib/graph/graph.ex
+++ lib/graph/graph.ex
@@ -1,3 +1,10 @@
 defmodule ExGraphs.Graph do
+  @moduledoc """
+  A struct representing a graph.
 
+  Has two maps as fields:
+    - vertices: a map of vertices in the graph (index of the vertex as key).
+    - edges: a map of edges in the graph ((u.index, v.index) as key).
+  """
+  defstruct [:vertices, :edges]
 end
