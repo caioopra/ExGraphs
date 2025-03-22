61a9d047db6fcaa521e937bffcbb589e67b4ac14
caioopra <caioprsilva@gmail.com>
2025-03-22 14:12:56 -0300

    feat: implement create_edge function to establish edges between vertices with error handling

--- lib/graph/edge.ex
+++ lib/graph/edge.ex
@@ -6,6 +6,23 @@ defmodule ExGraphs.Edge do
     %Edge{u: u, v: v, weight: weight}
     # where u and v are vertices and weight is the weight of the edge (optional).
   """
+  alias ExGraphs.Vertex
+
   @enforce_keys [:u, :v]
   defstruct [:u, :v, :weight]
+
+  @doc """
+  Creates an edge unidrected edge between two vertices.
+  Returns:
+    - `{:ok, edge, u, v}` if the edge was successfully created.
+    - `{:error, u, v}` if the vertices are the same or already has an edge between them.
+  """
+  def create_edge(%ExGraphs.Vertex{} = u, %ExGraphs.Vertex{} = v, weight \\ 1) do
+    {status, u, v} = Vertex.add_neighbor(u, v)
+
+    case status do
+      :ok -> {:ok, %ExGraphs.Edge{u: u, v: v, weight: weight}, u, v}
+      :error -> {:error, u, v}
+    end
+  end
 end
--- test/graph/edge_test.exs
+++ test/graph/edge_test.exs
@@ -1,14 +1,47 @@
 defmodule ExGraphsTest.EdgeTest do
   use ExUnit.Case
 
-  describe "Edge struct and methods tests" do
-    test "creates an Edge" do
-      u = %ExGraphs.Vertex{index: 1, label: "A"}
-      v = %ExGraphs.Vertex{index: 2, label: "B"}
-      edge = %ExGraphs.Edge{u: u, v: v}
+  alias ExGraphs.{Vertex, Edge}
 
-      assert edge.u == u
-      assert edge.v == v
+  describe "Edge struct and methods tests" do
+    IO.puts("Running Edge tests")
+
+    test "Edge.create_edge/2 creates an edge between two vertices" do
+      v1 = %Vertex{index: 1, label: "A"}
+      v2 = %Vertex{index: 2, label: "B"}
+
+      assert {:ok, edge, new_v1, new_v2} = Edge.create_edge(v1, v2)
+
+      assert edge.u.index == 1
+      assert edge.v.index == 2
+      assert edge.weight == 1
+
+      assert new_v1.neighbors == [2]
+      assert new_v2.neighbors == [1]
+    end
+
+    test "Edge.create_edge/2 creates an edge with a custom weight" do
+      v1 = %Vertex{index: 1, label: "A"}
+      v2 = %Vertex{index: 2, label: "B"}
+
+      assert {:ok, edge, _new_v1, _new_v2} = Edge.create_edge(v1, v2, 3.5)
+
+      assert edge.weight == 3.5
+    end
+
+    test "Edge.create_edge/2 returns error when trying to create a self-loop" do
+      v1 = %Vertex{index: 1, label: "A"}
+
+      assert {:error, v1, v1} = Edge.create_edge(v1, v1)
+    end
+
+    test "Edge.create_edge/2 returns error when creating an existing edge" do
+      v1 = %Vertex{index: 1, label: "A"}
+      v2 = %Vertex{index: 2, label: "B"}
+
+      {:ok, _edge, v1, v2} = Edge.create_edge(v1, v2)
+
+      assert {:error, ^v1, ^v2} = Edge.create_edge(v1, v2)
     end
   end
 end
