f9e80c7b7707a3695da7c719af0090f94ad17c48
caioopra <caioprsilva@gmail.com>
2025-03-22 14:33:42 -0300

    feat: add insert_vertex function to handle vertex insertion with error checking

--- lib/graph/graph.ex
+++ lib/graph/graph.ex
@@ -7,4 +7,22 @@ defmodule ExGraphs.Graph do
     - edges: a map of edges in the graph ((u.index, v.index) as key).
   """
   defstruct [:vertices, :edges]
+
+  @doc """
+  Inserts a vertex into the graph. If the vertex already exists, returns an error.
+
+  Returns:
+    - `{:ok, updated_graph}` if the vertex was successfully added.
+    - `{:error, graph}` if the vertex already exists.
+  """
+  def insert_vertex(%ExGraphs.Graph{} = graph, %ExGraphs.Vertex{} = vertex) do
+    cond do
+      Map.has_key?(graph.vertices, vertex.index) ->
+        {:error, graph}
+
+      true ->
+        updated_vertices = Map.put(graph.vertices, vertex.index, vertex)
+        {:ok, %ExGraphs.Graph{graph | vertices: updated_vertices}}
+    end
+  end
 end
