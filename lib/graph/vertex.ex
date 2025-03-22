62baa168c964629b270fc91d05a60144ac9b8c1f
caioopra <caioprsilva@gmail.com>
2025-03-22 14:03:26 -0300

    fix: enhance add_neighbor function to prevent self-loops and duplicate edges

--- lib/graph/vertex.ex
+++ lib/graph/vertex.ex
@@ -7,10 +7,29 @@ defmodule ExGraphs.Vertex do
 
   @doc """
   Adds an undirected edge between two vertices.
-  Returns the updated vertices, ensuring each references the updated version of the other.
+  Returns:
+  - `{:ok, updated_u, updated_v}` if the edge was successfully added.
+  - `{:error, u, v}` if they are already neighbors or the same vertex.
   """
   def add_neighbor(%ExGraphs.Vertex{} = u, %ExGraphs.Vertex{} = v) do
+    cond do
+      # prevent self-loops
+      u.index == v.index ->
+        {:error, u, v}
+
+      # won't duplicate entries
+      v.index in u.neighbors or u.index in v.neighbors ->
+        {:error, u, v}
+
+      true ->
+        {updated_u, updated_v} = update_neighbors(u, v)
+        {:ok, updated_u, updated_v}
+    end
+  end
+
+  defp update_neighbors(%ExGraphs.Vertex{} = u, %ExGraphs.Vertex{} = v) do
     updated_u = %ExGraphs.Vertex{u | neighbors: [v.index | u.neighbors], degree: u.degree + 1}
+
     updated_v = %ExGraphs.Vertex{v | neighbors: [u.index | v.neighbors], degree: v.degree + 1}
 
     {updated_u, updated_v}
