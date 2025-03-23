87c14ec5fede7a75e77356931dd1d807fcd67e53
caioopra <caioprsilva@gmail.com>
2025-03-23 12:10:55 -0300

    feat: adds type definition and spec for insert_vertex function and implement create_vertex

--- lib/graph/graph.ex
+++ lib/graph/graph.ex
@@ -8,6 +8,13 @@ defmodule ExGraphs.Graph do
   """
   defstruct [:vertices, :edges]
 
+  @type t :: %__MODULE__{
+          vertices: %{integer() => ExGraphs.Vertex.t()},
+          edges: %{integer() => ExGraphs.Edge.t()}
+        }
+
+  @spec insert_vertex(%ExGraphs.Graph{}, ExGraphs.Vertex.t()) ::
+          {:ok, %ExGraphs.Graph{}} | {:error, %ExGraphs.Graph{}}
   @doc """
   Inserts a vertex into the graph. If the vertex already exists, returns an error.
 
@@ -24,5 +31,17 @@ defmodule ExGraphs.Graph do
         updated_vertices = Map.put(graph.vertices, vertex.index, vertex)
         {:ok, %ExGraphs.Graph{graph | vertices: updated_vertices}}
     end
+  end
+
+  @doc """
+  Inserts an edge into the graph. If the edge already exists, returns an error.
+  """
+  def create_vertex(%ExGraphs.Graph{} = graph, index, label) do
+    vertex = %ExGraphs.Vertex{index: index, label: label}
+    insert_vertex(graph, vertex)
+  end
+
+  def create_vertex(%ExGraphs.Graph{} = graph, index) do
+    create_vertex(graph, index, to_string(index))
   end
 end
