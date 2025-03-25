d4fc843b5f5c8d325c85d83a4fa590b63e427085
caioopra <caioprsilva@gmail.com>
2025-03-25 10:18:30 -0300

    feat: adds documentation for create_vertex function on Graph

--- lib/graph/graph.ex
+++ lib/graph/graph.ex
@@ -5,6 +5,9 @@ defmodule ExGraphs.Graph do
   Has two maps as fields:
     - vertices: a map of vertices in the graph (index of the vertex as key).
     - edges: a map of edges in the graph ((u.index, v.index) as key).
+
+  Main methods:
+    - `create_vertex`: given the graph, an index and the label (optional), tries to inserts a graph into the graph
   """
   defstruct vertices: %{}, edges: %{}
 
@@ -33,14 +36,24 @@ defmodule ExGraphs.Graph do
     end
   end
 
+  @spec create_vertex(ExGraphs.Graph.t(), integer(), String.t()) ::
+          {:ok, %ExGraphs.Graph{edges: term(), vertices: term()}}
+          | {:error, %ExGraphs.Graph{edges: term(), vertices: term()}}
   @doc """
   Inserts an edge into the graph. If the edge already exists, returns an error.
+
+  Returns:
+    - `{:ok, updated_graph}` if the vertex was successfully addded
+    - `{:error, graph}` if couldn't insert it
   """
   def create_vertex(%ExGraphs.Graph{} = graph, index, label) do
     vertex = %ExGraphs.Vertex{index: index, label: label}
     insert_vertex(graph, vertex)
   end
 
+  @spec create_vertex(ExGraphs.Graph.t(), integer()) ::
+          {:ok, %ExGraphs.Graph{edges: term(), vertices: term()}}
+          | {:error, %ExGraphs.Graph{edges: term(), vertices: term()}}
   def create_vertex(%ExGraphs.Graph{} = graph, index) do
     create_vertex(graph, index, to_string(index))
   end
