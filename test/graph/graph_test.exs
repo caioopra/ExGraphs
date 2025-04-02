fe8e620440802c22b267cdd9d36e63c080ec8268
caioopra <caioprsilva@gmail.com>
2025-04-02 18:47:45 -0300

    test: add edge manipulation tests for create_edge function

--- test/graph/graph_test.exs
+++ test/graph/graph_test.exs
@@ -1,7 +1,7 @@
 defmodule ExGraphsTest.GraphTest do
   use ExUnit.Case
 
-  alias ExGraphs.{Vertex, Edge, Graph}
+  alias ExGraphs.{Vertex, Graph}
 
   describe "Vertex manipulation on Graph struct tests" do
     IO.puts("Running Graph tests for vertices manipulation")
@@ -60,7 +60,45 @@ defmodule ExGraphsTest.GraphTest do
       assert status == :ok
       assert ExGraphs.Graph.vertices_amount(graph) == 1
       assert Map.get(graph.vertices, 1).label == "1"
+    end
+  end
 
+  describe "Edge manipulation on Graph struct tests" do
+    setup do
+      graph = %Graph{}
+
+      graph =
+        graph
+        |> Graph.create_vertex(1, "A")
+        |> then(fn {:ok, g} -> g end)
+        |> Graph.create_vertex(2, "B")
+        |> then(fn {:ok, g} -> g end)
+        |> Graph.create_vertex(3, "C")
+        |> then(fn {:ok, g} -> g end)
+
+      %{graph: graph}
+    end
+
+    test "create_edge/4 should create a new edge between existing vertices", %{graph: graph} do
+      u = Graph.get_vertex(graph, 1)
+      v = Graph.get_vertex(graph, 2)
+
+      {status, graph, u, v} = Graph.create_edge(graph, u, v)
+
+      assert status == :ok
+      assert Map.has_key?(graph.edges, {1, 2})  # error here ?
+
+      assert u.degree == 1
+      assert v.degree == 1
+    end
+
+    test "create_edge/4 should fail when vertices don't exist", %{graph: graph} do
+      u = Graph.get_vertex(graph, 1)
+      vertex = %Vertex{index: 100, label: "Not in Graph"}
+
+      {status, _graph, _u, _v} = Graph.create_edge(graph, u, vertex, weight: 5)
+
+      assert status == :error
     end
   end
 end
