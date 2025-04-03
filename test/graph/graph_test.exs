fb75e2adeb69aa0b27ae91fe1e0fa40c8a06740d
caioopra <caioprsilva@gmail.com>
2025-04-03 18:35:15 -0300

    test: adds has_edge and edges_amount tests

--- test/graph/graph_test.exs
+++ test/graph/graph_test.exs
@@ -110,5 +110,49 @@ defmodule ExGraphsTest.GraphTest do
 
       assert status == :error
     end
+
+    test "has_edge/3 should return true for existing edge", %{graph: graph} do
+      u = Graph.get_vertex(graph, 1)
+      v = Graph.get_vertex(graph, 2)
+
+      {status, graph, u, v} = Graph.create_edge(graph, u, v)
+
+      assert status == :ok
+      assert Graph.has_edge?(graph, u, v) == true
+    end
+
+    test "has_edge/3 should return true for existing edge when searching with vertices IDs", %{graph: graph} do
+      u = Graph.get_vertex(graph, 1)
+      v = Graph.get_vertex(graph, 2)
+
+      {status, graph, u, v} = Graph.create_edge(graph, u, v)
+
+      assert status == :ok
+      assert Graph.has_edge?(graph, u.index, v.index) == true
+    end
+
+    test "has_edge/3 should return false for non-existing edge", %{graph: graph} do
+      u = Graph.get_vertex(graph, 1)
+      v = Graph.get_vertex(graph, 2)
+
+      assert Graph.has_edge?(graph, u, v) == false
+    end
+
+    test "edges_amount/1 should return the correct number of edges", %{graph: graph} do
+      u = Graph.get_vertex(graph, 1)
+      v = Graph.get_vertex(graph, 2)
+
+      {status, graph, _u, v} = Graph.create_edge(graph, u, v)
+
+      assert status == :ok
+      assert Graph.edges_amount(graph) == 1
+
+      w = Graph.get_vertex(graph, 3)
+
+      {status, graph, _u, _w} = Graph.create_edge(graph, v, w)
+
+      assert status == :ok
+      assert Graph.edges_amount(graph) == 2
+    end
   end
 end
